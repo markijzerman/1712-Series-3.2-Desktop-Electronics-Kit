713f5914e4ba015d879ab423f14ae6d826bb46ea
afrancey <adam.francey.af@gmail.com>
2017-06-30 11:49:27 -0500

    more comments

--- simple examples/Ir_led/Ir_led.ino
+++ simple examples/Ir_led/Ir_led.ino
@@ -1,25 +1,39 @@
-// IR sensor is plugged into port E on DM, DM plugged into port 1 on node
-// from 3.1 Device Module A schematics we see port E is connected to I/O line 4
-// from pinout diagram (or node controller schematics) we see port 1 line 4 is connect to Teensy pin A7
-const int kIrPin = A7; // or Port1.DMLow.getPin('E')
-const int kIrThreshold = 400;
+// Living Architecture Systems
+// ir_led.ino
+// Turns on an LED after passing a pre-defined proximity threshold
+
+// Hardware Setup:
+// one node controller, one low current device module
+// one LED, one IR sensor
+// Connect node controller and RPi over USB
+// Plug the device module into node controller port 1
+// Plug IR sensor into device module port E
+// Plug LED into device module port F
+
+//IR SENSOR SETUP
+
+// from 3.1 Device Module A schematics we see DM port E is connected to I/O pin 4
+// from pinout diagram (or node controller schematics) NC port 1 pin 4 is connected to Teensy pin A7
+// and if using node_ports library you'll find Port1.DMLow.getPin('E') == A7
+int ir_pin = A7;
+int ir_threshold = 400;
 bool ir_trigger = false;
 int ir_value = 0;
 
-// similarly, an LED on port F is controlled by Teensy pin 6
-const int kLedPin = 6; //or Port1.DMLow.getPin('F')
+// LED SETUP
+// from 3.1 Device Module A schematics we see DM port F is connected to DM pin 3
+// from pinout diagram (or node controller schematics) NC port 1 pin 3 is connected to Teensy pin 6
+// and if using node_ports library you'll find Port1.DMLow.getPin('F') == 6
+int led_pin = 6;
 
 
 void setup() {
-  Serial.begin(9600);
-
+  pinMode(ir_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
 }
 
 void loop() {
-  ir_value = analogRead(kIrPin);
+  ir_value = analogRead(ir_pin);
   ir_trigger = ir_value > 400;
-
-  digitalWrite(kLedPin, ir_trigger);
-  Serial.write("IR sensor triggered");
-
+  digitalWrite(led_pin, ir_trigger);
 }
--- simple examples/wav_mic/wav_mic.ino
+++ simple examples/wav_mic/wav_mic.ino
@@ -1,45 +1,70 @@
-// WAV Trigger commands
-#define TRK_PLAY_SOLO 0
-#define TRK_PLAY_POLY 1
-#define TRK_STOP 4
+// Living Architecture Systems
+// wav_mic.ino
+// Plays a sound after passing a pre-defined volume threshold
+// Bonus example: it can get tricky, but never impossible, to manually find pin numbers
 
+// Hardware Setup:
+// One node controller, one low current device module
+// One WAV Trigger and speaker, one sound detector
 
-// microphone on port B
-//wav trigger on port a
+// Plug the device module into node controller port 2
+// Plug WAV trigger into device module port A
+// Plug sound detector into device module port B
 
-// based on cable diagrams, we know env pin maps onto portBline2
-// aud maps into portBline3
-const int kEnveloplePin =  A19; // = Port2.DMLow.getPin('B',2)
-const int kAudioPin = A18; // = Port2.DMLow.getPin('B',3)
+// SOUND DETECTOR SETUP
+
+// DM port B has two data pins: DM port B pin 2 and DM port B pin 3
+// from 3.1 Device Module A schematics we see DM port B pin 2 = DM pin 8 and DM port B pin 3 = DM pin 7
+// from pinout diagram (or node controller schematics) NC port 2 pin 8 is connected to Teensy pin A19
+// and NC port 2 pin 7 is connected to Teensy pin A18
+// and if using node_ports library you'll find that
+// Port2.DMLow.getPin('B',2) == A19
+// Port2.DMLow.getPin('B',3) == A18
+
+// sound detector cable mapping shows
+// envelope == DM port B pin 2
+// audio == DM port B pin 3
+
+// Therefore, 
+int envelope_pin =  A19;
+int audio_pin = A18;
+
+// rest of variables for sound detector
 int envelope_value = 0;
 int envelope_trigger = 0;
 int envelope_threshold = 700;
 
-const int kRxPin = 9; // = Port2.DMLow.getPin('A',3)
-const int kTxPin = 10;// = Port2.DMLow.getPin('A',2)
 
-SoftwareSerial WAVSerial(kRxPin,kTxPin);
+// WAV TRIGGER SETUP
 
+// DM port A has two data pins: DM port A pin 2 and DM port A pin 3
+// from 3.1 Device Module A schematics we see DM port B pin 2 = DM pin 2 and DM port B pin 3 = DM pin 1
+// from pinout diagram (or node controller schematics) NC port 2 pin 2 is connected to Teensy pin 10
+// and NC port 2 pin 1 is connected to Teensy pin 9
+// and if using node_ports library you'll find that
+// Port2.DMLow.getPin('A',2) == 10
+// Port2.DMLow.getPin('A',3) == 9
 
+// WAV trigger cable mapping shows
+// WAV rx == DM port B pin 2
+// WAV tx == DM port B pin 3
 
-void setup() {
-  Serial.begin(9600);
-  WAVSerial.begin(57600);
+// Therefore (REMINDER: controller rx connects to WAV trigger tx, vice versa),
+int rx_pin = 9;
+int tx_pin = 10;
 
-}
+// WAV triggers are controlled by a serial connection
+// Not by coincidence, Teensy pins 9 and 10 can be used as hardware Serial2
+// However, a SoftwareSerial is limited but has enough function to control a WAV trigger
+// if not using hardware serial pins the following line will become SoftwareSerial WAVSerial(rx_pin,tx_pin);
+#define WAVSerial Serial2
 
-void loop() {
-  envelope_value = analogRead(kEnvelopePin);
-  envelope_trigger = envelope_value > envelope_threshold;
+// WAV Trigger commands (usually, these are defined in a separate header file)
+#define TRK_PLAY_SOLO 0
+#define TRK_PLAY_POLY 1
+#define TRK_STOP 4
 
-  if (envelope_trigger){
-    trackControl(1,TRK_PLAY_SOLO);
-  }
-  
-
-}
-
-// trackControl function - see WAV trigger library
+// trackControl function - see WAV trigger library for details
 void trackControl(int trk, int code){
   uint8_t txbuf[8];
   txbuf[0] = 0xf0; // SOM1;
@@ -50,9 +75,23 @@ void trackControl(int trk, int code){
   txbuf[5] = (uint8_t)trk;
   txbuf[6] = (uint8_t)(trk>>8);
   txbuf[7] = 0x55; // EOM;
-
   WAVSerial.write(txbuf,8);
-
 }
+
+
+void setup() {
+  // start the serial port for the WAV trigger
+  WAVSerial.begin(57600);
+}
+
+void loop() {
+  envelope_value = analogRead(envelope_pin);
+  envelope_trigger = envelope_value > envelope_threshold;
+  if (envelope_trigger){
+    trackControl(1,TRK_PLAY_SOLO);
+  }
+}
+
+
 
 
